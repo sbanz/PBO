f78357f86f8d3a57eeaaf925640cf6377cbdd7a9
sbanz <sabanz0210@gmail.com>
2023-03-07 00:59:27 +0800

    posttest 1

---
+++ posttest1/src/data_klub.java
@@ -0,0 +1,12 @@
+
+public class data_klub {
+    public String clname;
+    public String clstadium;
+    public int playerttl;
+
+    data_klub(String clname, String clstadium, int playerttl){
+        this.clname = clname;
+        this.clstadium = clstadium;
+        this.playerttl = playerttl;
+    }
+}
