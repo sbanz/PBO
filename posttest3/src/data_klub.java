49b1434a476d28b11d6144fa73b1ba9a170c1ade
sbanz <sabanz0210@gmail.com>
2023-03-21 00:40:48 +0800

    posttest 3

---
+++ posttest3/src/data_klub.java
@@ -0,0 +1,32 @@
+class data_klub {
+    private String clname;
+    private String clstadium;
+    private String hometown;
+
+    public data_klub(String clname, String clstadium, String hometown){
+        this.clname = clname;
+        this.clstadium = clstadium;
+        this.hometown = hometown;
+    }
+
+    public String getclname(){
+        return clname;
+    }
+    public void setclname(String clname){
+        this.clname = clname;
+    }
+
+    public String getclstadium(){
+        return clstadium;
+    }
+    public void setclstadium(String clstadium){
+        this.clstadium = clstadium;
+    }
+
+    public String gethometown(){
+        return hometown;
+    }
+    public void sethometown(String hometown){
+        this.hometown = hometown;
+    }
+}
---
+++ posttest3/src/first_team.java
@@ -0,0 +1,16 @@
+public class first_team extends data_klub{
+    private int total_player;
+
+    public first_team(String clname, String clstadium, String hometown, int total_player) {
+        super(clname, clstadium, hometown);
+        this.total_player = total_player;
+    }
+
+    public int gettotal_player(){
+        return total_player;
+    }
+
+    public void settotal_player(int total_player){
+        this.total_player = total_player;
+    }
+}
