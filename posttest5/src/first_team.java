9a4e56579aaf2b0aa871454212142874fc9648e5
sbanz <sabanz0210@gmail.com>
2023-04-05 15:15:22 +0800

    Posttest 5

---
+++ posttest5/src/first_team.java
@@ -0,0 +1,24 @@
+public class first_team extends data_klub{
+    private int total_player;
+
+    public first_team(String clname, String clstadium, String hometown, int total_player) {
+        super(clname, clstadium, hometown);
+        this.total_player = total_player;
+    }
+
+    public int gettotal_player(){
+        return total_player;
+    }
+
+    public void settotal_player(int total_player){
+        this.total_player = total_player;
+    }
+
+    public void done_add(){
+        System.out.println("First Team data has been added, with total player of : "+ this.total_player);
+    }
+
+    public void done_add(String clname){
+        System.out.println("You just added "+clname+ " data");
+    }
+}
